468148d8b4cd41782571e97239afb790e3855ab1
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-12-26 14:18:54 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,6 +1,5 @@
-############################################
+
 # VPC
-############################################
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
@@ -8,16 +7,14 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 }
 
-############################################
 # Internet Gateway
-############################################
+
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 }
 
-############################################
 # Public Subnets (ALB + NAT)
-############################################
+
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -27,29 +24,27 @@ resource "aws_subnet" "public" {
 
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.4.0/24"
+  cidr_block              = "10.0.2.0/24"
   availability_zone       = "ap-south-1b"
   map_public_ip_on_launch = true
 }
 
-############################################
 # Private Subnets (ECS + RDS)
-############################################
+
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-south-1b"
 }
 
 resource "aws_subnet" "private_2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.3.0/24"
+  cidr_block        = "10.0.4.0/24"
   availability_zone = "ap-south-1c"
 }
 
-############################################
 # Public Route Table → Internet Gateway
-############################################
+
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -60,7 +55,7 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public_1" {
-  subnet_id      = aws_subnet.public.id
+  subnet_id      = aws_subnet.public_1.id
   route_table_id = aws_route_table.public.id
 }
 
@@ -69,9 +64,9 @@ resource "aws_route_table_association" "public_2" {
   route_table_id = aws_route_table.public.id
 }
 
-############################################
+
 # NAT Gateway (CRITICAL FIX)
-############################################
+
 resource "aws_eip" "nat" {
   domain = "vpc"
 }
@@ -84,9 +79,8 @@ resource "aws_nat_gateway" "nat" {
   depends_on = [aws_internet_gateway.igw]
 }
 
-############################################
 # Private Route Table → NAT Gateway
-############################################
+
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
