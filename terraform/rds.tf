cf37709bddfc6a7faebaf4d9e05ab399c1e07d00
Ķábîłáň § <143497959+Kabilan2370@users.noreply.github.com>
2025-12-26 14:15:14 +0530

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,4 +1,16 @@
 
+resource "aws_db_subnet_group" "strapi" {
+  name       = "docker-strapi-db-subnet-group"
+  subnet_ids = [
+    aws_subnet.private_1.id,
+    aws_subnet.private_2.id
+  ]
+
+  tags = {
+    Name = "db-subnet-group"
+  }
+}
+
 resource "aws_db_instance" "postgres" {
   engine            = "postgres"
   engine_version    = "15"
